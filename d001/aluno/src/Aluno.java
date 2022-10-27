2199b8d6a5f21056f564b4a9c99d54f879ed9c96
Felipe Inácio <115639983+lipeinacio@users.noreply.github.com>
2022-10-26 21:19:39 -0300

    Add files via upload

---
+++ d001/aluno/src/Aluno.java
@@ -0,0 +1,22 @@
+
+public class Aluno {
+    public static Aluno a1;
+    String nome;
+    String endereco; 
+    long telefone;
+    String email;
+    String matricula;
+
+
+    void status() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Endereço: " + this.endereco);
+        System.out.println("Telefone: " + this.telefone);
+        System.out.println("E-mail: " + this.email);
+        System.out.println("Matricula: " + this.matricula);
+
+    }
+    
+    
+}
+
---
+++ d001/aluno/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        
+        a1.nome = "Beltrano";
+        a1.endereco = "Rua Tal de Tal 42-1";
+        a1.telefone = 977445773;
+        a1.email = "beltranodetal@gmail.com";
+        a1.matricula = "Efetivada";
+        a1.status(); 
+    }
+
+}
